f2a2167965850330785076dc3a5eed474a225086
Bryan Liu <bry@tradeshift.com>
2021-03-07 17:13:10 +0800

    remove the unnecessary import format file

--- algorithm/Merge2SortedArray.py
+++ algorithm/Merge2SortedArray.py
@@ -1,9 +1,17 @@
 import json
 import unittest
 
-from algorithm.base import * #如果在目录里面要加上目录的名称，也就是package的名称
 
 class Solution(object):
+    """
+    LC 88. 合并两个有序数组 link: https://leetcode-cn.com/problems/merge-sorted-array/
+
+    给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+    初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+    """
+
     def merge(self, nums1, m, nums2, n):
         """
         :type nums1: List[int]
@@ -29,9 +37,9 @@ class Solution(object):
 
 class Test(unittest.TestCase):
 
- def test_case(self):
-     s = Solution()
-     self.assertEqual([2, 5, 6, 0], s.merge([0,0,0,0], 0, [2,5,6],3))
+    def test_case(self):
+        s = Solution()
+        self.assertEqual([2, 5, 6, 0], s.merge([0, 0, 0, 0], 0, [2, 5, 6], 3))
 
 
 if __name__ == '__main__':
