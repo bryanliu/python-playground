6687c5a20b594b6b1c16fbb4fd86137f2b479792
Bryan Liu <bry@tradeshift.com>
2021-03-21 17:04:21 +0800

    add file iteration in folder and sub folder and count the line numbers

--- basicpractice/file_statistics.py
+++ basicpractice/file_statistics.py
@@ -15,11 +15,11 @@ def count_line(file):
 def count_file_in_path(path):
     res = []
     for root, dirs, files in os.walk(path):
-        # for d in dirs:
-        #     print(d)
-        for f in files:
+        for d in dirs: #find the sub folder first
+            res += count_file_in_path(root+ '//' + d)
+        for f in files: #count the files in current folder
             lines = count_line(root + "//" + f)
-            print(f, "lines: " , lines)
+            print(root + "//"+ f, "lines: " , lines)
             res.append(f)
             res.append(lines)
         return res
@@ -29,7 +29,7 @@ def count_file_in_path(path):
 class ut(unittest.TestCase):
 
     def test_read_test_folder(self):
-        self.assertEquals(["a", 3, "b", 4], count_file_in_path("../test"))
+        self.assertEquals(['d', 4, 'c', 1, "a", 3, "b", 4], count_file_in_path("../test"))
 
 
 if __name__ == '__main__':
