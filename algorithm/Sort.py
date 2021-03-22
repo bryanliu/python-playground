61642a60fb2e49591c4f0d8a8bddee22ed667c92
Bryan Liu <bry@tradeshift.com>
2021-03-22 13:57:33 +0800

    format code

--- algorithm/Sort.py
+++ algorithm/Sort.py
@@ -9,17 +9,18 @@ def bubblesort(arr):
 
     return arr
 
+
 def insertsort(arr):
     print("insert sort before: ", arr)
     for i in range(1, len(arr)):
-        if arr[i] < arr[i-1]:
+        if arr[i] < arr[i - 1]:
             tmp = arr[i]
-            j = i-1
+            j = i - 1
             while arr[j] > tmp:
-                arr[j+1] = arr[j]
+                arr[j + 1] = arr[j]
                 j -= 1
 
-            arr[j+1] = tmp
+            arr[j + 1] = tmp
     print("insert sort after: ", arr)
     return arr
 
@@ -28,7 +29,6 @@ class ut(unittest.TestCase):
 
     def seUp(self):
         pass
-
 
     def test_bubble_sort_success(self):
         arr = [3, 4, 5, 6, 7, 2, 1, 33, 3, 6, 8, 9, 2, 1]
