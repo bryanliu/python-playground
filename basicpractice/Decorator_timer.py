912cd2f7711fde6590d81a9efbf92ccfe3c68415
Bryan Liu <bry@tradeshift.com>
2021-04-02 21:57:24 +0800

    first decorator

---
+++ basicpractice/Decorator_timer.py
@@ -0,0 +1,27 @@
+import functools
+import time
+
+"""
+使用装饰器 + 闭包 实现一个显示运行时间的装饰器
+"""
+
+
+def timer(func):
+    def wapper(*param):
+        start = time.time()
+        func(param)
+        end = time.time()
+        print(f"spend {end - start} to execute the function")
+
+    return wapper
+
+
+@timer
+@functools.lru_cache(None)
+def target_func(param):
+    print(f"I'm in function with paramaters: {param}")
+    time.sleep(1)
+
+
+target_func("a", "b")
+# >> spend 1.0040099620819092 to execute the function
